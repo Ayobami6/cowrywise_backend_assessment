9ee1e67f4cb33403e50a72a760d36a7a68ccac7c
Ayobami Alaran A. <ayobamidele006@gmail.com>
2024-09-18 02:18:45 +0100

    feat: Enable admin management for library models
    Registers the User, Book, Category, and BorrowedBookLog models with the Django admin interface. This allows for convenient data management and oversight through the admin panel. The BorrowedBookLog model is customized to display relevant borrowing information.

--- admin_api/api/admin.py
+++ admin_api/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import User, Book, BorrowedBookLog, Category
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Book)
+admin.site.register(Category)
+
+@admin.register(BorrowedBookLog)
+class BorrowedBookLogAdmin(admin.ModelAdmin):
+    list_display = ('borrower', 'book', 'borrow_date', 'return_date')
